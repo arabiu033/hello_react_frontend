5b5d7df8051de0a3d37bb46daab4920d0b84a488
Aminu Rabiu <arabiu033@gmail.com>
2023-02-22 08:58:53 +0100

    first commit

---
+++ src/greeting.js
@@ -0,0 +1,21 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchData } from './redux/greetings';
+
+function Greet() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchData());
+  }, [dispatch]);
+
+  const welcome = useSelector((state) => state.greetings);
+  return (
+    <>
+      <h1>{ welcome }</h1>
+      <button onClick={() => dispatch(fetchData())}> Change Greetibg </button>
+    </>
+  )
+}
+
+export default Greet;
